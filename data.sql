0d6795d4c74bb1e569189bab82fcbfa7080a909b
Janjaran <145137082+Janjaran@users.noreply.github.com>
2024-11-04 15:48:55 +0800

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    address VARCHAR(255),
+    age INT;
+);
+
+
+CREATE TABLE Members (
+    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    member_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone_number VARCHAR(15) NOT NULL,   
+    FOREIGN KEY (added_by) REFERENCES Users(user_id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE Games (
+    game_id INT AUTO_INCREMENT PRIMARY KEY,
+    game_name VARCHAR(100) NOT NULL,
+    member_id INT,
+    added_by INT,  
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_by INT,
+    FOREIGN KEY (member_id) REFERENCES Members(member_id) ON DELETE CASCADE,
+    FOREIGN KEY (added_by) REFERENCES Users(user_id) ON DELETE SET NULL
+);
+
